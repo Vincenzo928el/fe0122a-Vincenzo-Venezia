dd3a7fd81fdd20ff43e1e7c7dcb3245be76366f4
Vincenzo928el <vincenzovenezia5@gmail.com>
2022-02-15 12:51:02 +0100

    aggiornamento-cartella

---
+++ s4/g2/assets/js/myscript.js
@@ -0,0 +1,20 @@
+var variabile = 30;
+
+{
+    let variabile = 'Vincenzo';
+}
+console.log(variabile);
+
+const solo = 'Vincenzo';
+console.log(solo);
+
+variabile += solo;
+console.log(variabile);
+
+
+var numero1 = 60;
+var numero2 = 5;
+var numero3 = 40;
+
+numero1 += numero2 - numero3;
+console.log(numero1)
