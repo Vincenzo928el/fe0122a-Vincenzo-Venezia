e25fea08d1ef22ec04c656241cea63d0986a6160
Vincenzo928el <vincenzovenezia5@gmail.com>
2022-02-17 19:09:33 +0100

    calcolatrice-progetto

---
+++ s4/progetto/assets/js/myscript.js
@@ -0,0 +1,16 @@
+function resetCalcolatrice(){
+    let risultato = document.getElementById("risultato")
+    risultato.value = ''
+}
+
+function totale(){
+    let res = document.getElementById("risultato")
+    let totale = eval(res.value)
+    res.value = totale
+}
+
+function prendiTasto(tasto){
+    let risultato = document.getElementById("risultato")
+    let num = tasto.id;
+    risultato.value += num 
+}
