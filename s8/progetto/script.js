07952f74a4771fad1791a51f82c20f18d66af096
Vincenzo928el <vincenzovenezia5@gmail.com>
2022-03-17 21:51:17 +0100

    aggiornamento-progetto

--- s8/progetto/script.js
+++ s8/progetto/script.js
@@ -3,7 +3,6 @@ fetch('Abbigliamento.json')
     .then(function (data) {
     data.forEach(function (item) {
         var capi = new Vestiti(item.id, item.codprod, item.collezione, item.capo, item.modello, item.quantita, item.colore, item.prezzoivaesclusa, item.prezzoivainclusa, item.disponibile, item.saldo);
-        //console.log(abb.mostraModello())
         console.log(capi);
     });
 });
@@ -21,34 +20,27 @@ var Vestiti = /** @class */ (function () {
         this.disponibile = disponibile;
         this.saldo = saldo;
     }
-    /*mostraModello():string{
-        return `Il modello è ${this.modello}`
-    }*/
     Vestiti.prototype.getSaldoCapo = function () {
-        return Math.ceil(this.prezzoivainclusa * this.saldo / 100);
+        return (this.prezzoivainclusa * this.saldo / 100).toFixed(2);
     };
-    /*getSaldoCapo(sconto:number):any{
-        sconto = this.saldo
-        return this.prezzoivainclusa * sconto / 100
-    }*/
     Vestiti.prototype.getAcquistoCapo = function () {
         var sconto = this.prezzoivainclusa * this.saldo / 100;
-        return Math.ceil(this.prezzoivaesclusa - sconto);
+        return (this.prezzoivainclusa - sconto).toFixed(2);
     };
     return Vestiti;
 }());
 //CAPO 1
 var capo1 = new Vestiti(1, 2121, "primavera", "cardigan", 1231, 5, "nero", 18.50, 22.57, "negozio", 45);
 console.log(capo1);
-console.log("Lo sconto \u00E8 di + capo1.getSaldoCapo() + \u20AC");
-console.log("Il prezzo scontato \u00E8 di + capo1.getAcquistoCapo() + \u20AC");
+console.log("Lo sconto è di " + capo1.getSaldoCapo() + "€");
+console.log("Il prezzo scontato è di " + capo1.getAcquistoCapo() + "€");
 //CAPO 2
 var capo2 = new Vestiti(2, 4111, "estate", "t-shirt", 1251, 6, "rosso", 5.50, 6.71, "magazzino", 30);
 console.log(capo2);
-console.log("Lo sconto \u00E8 di + capo2.getSaldoCapo() + \u20AC");
-console.log("Il prezzo scontato \u00E8 di + capo2.getAcquistoCapo() + \u20AC");
+console.log("Lo sconto è di " + capo2.getSaldoCapo() + "€");
+console.log("Il prezzo scontato è di " + capo2.getAcquistoCapo() + "€");
 //CAPO 3
 var capo3 = new Vestiti(3, 1181, "inverno", "felpa", 1229, 8, "beige", 17.50, 21.35, "negozio", 50);
 console.log(capo3);
-console.log("Lo sconto \u00E8 di + capo3.getSaldoCapo() + \u20AC");
-console.log("Il prezzo scontato \u00E8 di + capo3.getAcquistoCapo() + \u20AC");
+console.log("Lo sconto è di " + capo3.getSaldoCapo() + "€");
+console.log("Il prezzo scontato è di " + capo3.getAcquistoCapo() + "€");
--- s8/progetto/script.ts
+++ s8/progetto/script.ts
@@ -10,17 +10,17 @@ fetch('Abbigliamento.json')
 
 class Vestiti{
 
-    id: number
-    codprod: number
-    collezione: string
-    capo: string
-    modello: number
-    quantita: number
-    colore: string
-    prezzoivaesclusa: number
-    prezzoivainclusa: number
-    disponibile: string
-    saldo: number
+    public id: number
+    public codprod: number
+    public collezione: string
+    public capo: string
+    public modello: number
+    public quantita: number
+    public colore: string
+    public prezzoivaesclusa: number
+    public prezzoivainclusa: number
+    public disponibile: string
+    public saldo: number
 
     constructor(id: number, codprod: number, collezione: string, capo: string, modello: number, quantita: number, colore: string, prezzoivaesclusa: number, prezzoivainclusa: number, disponibile: string, saldo: number){
         this.id = id
@@ -36,31 +36,30 @@ class Vestiti{
         this.saldo = saldo
     }
 
-    getSaldoCapo():any{
+    public getSaldoCapo():any{
         return (this.prezzoivainclusa * this.saldo / 100).toFixed(2)
     }
 
-
-    getAcquistoCapo():any{
+    public getAcquistoCapo():any{
         let sconto = this.prezzoivainclusa * this.saldo / 100
-        let prezzo = this.prezzoivainclusa
-        return (prezzo - sconto).toFixed(2)
+        return (this.prezzoivainclusa - sconto).toFixed(2)
     }
 }
+
 //CAPO 1
-let capo1= new Vestiti (1,2121,"primavera","cardigan",1231,5,"nero",18.50,22.57,"negozio",45)
+let capo1 = new Vestiti (1,2121,"primavera","cardigan",1231,5,"nero",18.50,22.57,"negozio",45)
 console.log(capo1)
-console.log(`Lo sconto è di + capo1.getSaldoCapo() + €`)
-console.log(`Il prezzo scontato è di + capo1.getAcquistoCapo() + €`)
+console.log("Lo sconto è di " + capo1.getSaldoCapo() + "€")
+console.log("Il prezzo scontato è di " + capo1.getAcquistoCapo() + "€")
 
 //CAPO 2
-let capo2= new Vestiti (2,4111,"estate","t-shirt",1251,6,"rosso",5.50,6.71,"magazzino",30)
+let capo2 = new Vestiti (2,4111,"estate","t-shirt",1251,6,"rosso",5.50,6.71,"magazzino",30)
 console.log(capo2)
-console.log(`Lo sconto è di + capo2.getSaldoCapo() + €`)
-console.log(`Il prezzo scontato è di + capo2.getAcquistoCapo() + €`)
+console.log("Lo sconto è di " + capo2.getSaldoCapo() + "€")
+console.log("Il prezzo scontato è di " + capo2.getAcquistoCapo() + "€")
 
 //CAPO 3
-let capo3= new Vestiti (3,1181,"inverno","felpa",1229,8,"beige",17.50,21.35,"negozio",50)
+let capo3 = new Vestiti (3,1181,"inverno","felpa",1229,8,"beige",17.50,21.35,"negozio",50)
 console.log(capo3)
-console.log(`Lo sconto è di + capo3.getSaldoCapo() + €`)
-console.log(`Il prezzo scontato è di + capo3.getAcquistoCapo() + €`)
+console.log("Lo sconto è di " + capo3.getSaldoCapo() + "€")
+console.log("Il prezzo scontato è di " + capo3.getAcquistoCapo() + "€")
